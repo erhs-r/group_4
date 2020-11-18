f07966b6c3ad0267d7dfcfadeae3fc1b60f71438
Elizabeth Morris <elizabeth.morris@colostate.edu>
2020-11-18 11:04:14 -0700

    Filtered for Colorado

--- data_raw.R
+++ data_raw.R
@@ -1,3 +1,4 @@
 library(tidyverse)
 
-counties <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+counties <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv") %>%
+  filter(state == "Colorado")
