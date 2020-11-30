786c9e3ffebd80cc3094c7e4a0fe1695917fe7e3
Ariel Timkovich <ariel.timkovich@gmail.com>
2020-11-30 10:16:03 -0700

    Filter Colorado

--- data/COVID data.R
+++ data/COVID data.R
@@ -3,3 +3,8 @@
 library(tidyverse)
 
 counties <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+
+counties_CO <- counties %>% 
+  filter(state == "Colorado")
+
+head(counties_CO)
