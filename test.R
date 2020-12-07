f9189129cdd92e6246e6147fb8f07120466adc65
Ariel Timkovich <ariel.timkovich@gmail.com>
2020-12-07 10:53:54 -0700

    added outbreak link

--- test.R
+++ test.R
@@ -3,3 +3,6 @@
 #elizabeth is testing if this works too 11/18
 
 #another test
+
+#Brooke gave us think link of outbreaks 
+##https://cdphe.maps.arcgis.com/apps/webappviewer/index.html?id=dcc0b993632a4bc68dc7b9a1dd015cfe
