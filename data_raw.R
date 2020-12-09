f84e3c2aa04108c9c41a2cf008dc1375b275e7ba
Emily Fitzmeyer <emily.fitzmeyer@colostate.edu>
2020-12-08 19:50:33 -0700

    notes

--- data_raw.R
+++ data_raw.R
@@ -2,3 +2,18 @@ library(tidyverse)
 
 counties <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv") %>%
   filter(state == "Colorado")
+
+## Dashboard:  
+## Map of CO
+## Map of CO counties (cases by county)
+## Get population data and look at cases by population 
+## Epi curves, adding reference lines for 4th of July, Halloween, Thanksgiving 
+## Overlay hospitalizations on the epi curve.
+
+## use lag() function for get new cases instead of cumulative cases. group_by, mutate(new_cases = cases - lag(cases))
+
+## specify in text where each dataset comes from in the text (even if they're merged).
+
+## use package tidycensus to get census api and pull population data
+
+## county boundaries from tigris 
