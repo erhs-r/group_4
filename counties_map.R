49e6186ab22ecca4db9112908c1bcf6ef057413b
Elizabeth Morris <elizabeth.morris@colostate.edu>
2020-12-13 14:20:43 -0700

    Made map stuff title case

--- counties_map.R
+++ counties_map.R
@@ -45,9 +45,9 @@ county_bins <- tibble(county = c("Larimer", "Mesa", "Weld", "Boulder",
                                 "frontier","frontier","frontier","frontier",
                                 "frontier","frontier","frontier"))
 
-rate_map <- full_join(co_counties, cdphe, by = "county") %>% #join cdphe data to SF
-  filter(Date == "11/11/2020") %>% #filter for Nov 11th
-  filter(Metric == "Rate Per 100,000") #filter for rate
+rate_map <- full_join(co_counties, cdphe, by = "county") #%>% #join cdphe data to SF
+  #filter(Date == "11/11/2020") %>% #filter for Nov 11th
+  #filter(Metric == "Rate Per 100,000") #filter for rate
 
 cdphe_class <- full_join(rate_map, county_bins, by = "county")
 
